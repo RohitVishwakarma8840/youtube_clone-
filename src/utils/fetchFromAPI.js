eae4323e18cd526d84ef8fc07752ba090a82e882
Rohit Vishwakarma <rohit8840@gmail.com>
2024-03-27 20:50:57 +0530

    adding second commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '86fc0b73f0msh3056920a3e54546p1a8a04jsne753257cb11c',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
